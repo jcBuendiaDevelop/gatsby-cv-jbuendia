285fa29cf35587f4b06951174b2999d0d62d6d31
Your Name <you@example.com>
2020-12-22 14:35:29 -0600

    master

---
+++ data/siteConfig.js
@@ -0,0 +1,133 @@
+module.exports = {
+  siteTitle: 'Hola! yo soy Juan Carlos!',
+  siteDescription: `Create your online curriculum in just a few minutes with this starter`,
+  keyWords: ['gatsbyjs', 'react', 'curriculum'],
+  authorName: 'Juan Carlos Buendia',
+  twitterUsername: '',
+  githubUsername: 'jcBuendiaDevelop',
+  authorAvatar: '/images/avatar.jpeg',
+  authorDescription: `Profesional de desarrollo de software altamente cualificado con más de 6 años de experiencia en diseño, desarrollo e integración de software. Conocimiento avanzado en SQL y PL/SQL, React, React-native, Nodejs, JavaScript, Scripting.
+  <br />
+  Experto desarrollador con habilidades de recopilación, análisis y gestión de datos. Trabaja bien bajo presión y cumple constantemente los plazos de entrega y objetivos sin descuidar por ello la calidad del trabajo.`,
+  skills: [
+    {
+      name: 'SQL & NoSQL',
+      level: 70,
+    },
+    {
+      name: 'NodeJs',
+      level: 70,
+    },
+    {
+      name: 'CSS',
+      level: 60,
+    },
+    {
+      name: 'Javascript',
+      level: 80,
+    },
+    {
+      name: 'React',
+      level: 90,
+    },
+    {
+      name: 'Git',
+      level: 60,
+    },
+    /* more skills here */
+  ],
+  jobs: [
+    {
+      company: 'Springlabs México',
+      begin: {
+        month: 'jun ',
+        year: '2019',
+      },
+      duration: 'Actual',
+      occupation: 'Líder de Área ',
+      description:
+        'Responsable del area de desarrollo, gestion de proyectos arquitecturas de software basadas en nube aws.',
+    },
+    {
+      company: 'TRONICO GROUP',
+      begin: {
+        month: 'Jun ',
+        year: '2018',
+      },
+      duration: '1 año',
+      occupation: 'líder Tecnico',
+      description:
+        'Responsable de la desarrollo, implementación  y parametrización del sistema de cotizador de productos y atencion a clientes, atravez de servicios en la nube de amazon.',
+    },
+    {
+      company: 'MAPFRE MEXICO',
+      begin: {
+        month: 'May',
+        year: '2017',
+      },
+      duration: '1 año',
+      occupation: ' Desarrollador Fullstack',
+      description:
+        'En la corporacion fui el encargado de la migracion de el sistema de impresion de polizas, ',
+    },
+    {
+      company: 'Grupo IAMSA (Telepeaje Dinámico)',
+      begin: {
+        month: 'Jun ',
+        year: '2016',
+      },
+      duration: '1 año',
+      occupation: 'Ingeniero de Desarrollo',
+      description:
+        ' Incorporé, probé e implementé productos de software modulares de alta disponibilidad y escalables, para el proyecto de interoperabilidad y clientes.',
+    },
+    {
+      company: 'Itesoluciones',
+      begin: {
+        month: 'Ene',
+        year: '2015',
+      },
+      duration: '1 años 6 meses',
+      occupation: 'Database developer',
+      description:
+        'Forme parte de la corporacion trabjando para el cliente de telepeaje dinamico como desarrollador de base de datos para el proyecto de cobro y facturacion en linea',
+    },
+  ],
+  portifolio: [
+    {
+      image: '/images/gatsby-starter-cv.png',
+      description: 'Gatsby starter CV template',
+      url:
+        'https://www.gatsbyjs.org/starters/santosfrancisco/gatsby-starter-cv/',
+    },
+    {
+      image: '/images/awesome-grid.png',
+      description: 'Responsive grid for ReactJS',
+      url: 'https://github.com/santosfrancisco/react-awesome-styled-grid',
+    },
+    /* more portifolio items here */
+  ],
+  social: {
+    twitter: '',
+    linkedin:
+      'https://www.linkedin.com/in/juan-carlos-buendia-prieto-472b1a144/',
+    github: 'https://github.com/jcBuendiaDevelop',
+    email: 'juan.carlosutninfo@gmail.com',
+  },
+  siteUrl: 'https://santosfrancisco.github.io/gatsbystarter-cv',
+  pathPrefix: '/gatsby-starter-cv', // Note: it must *not* have a trailing slash.
+  siteCover: '/images/cover.jpg',
+  googleAnalyticsId: 'UA-000000000-1',
+  background_color: '#ffffff',
+  theme_color: '#25303B',
+  fontColor: '#000000cc',
+  enableDarkmode: true, // If true, enables dark mode switch
+  display: 'minimal-ui',
+  icon: 'src/assets/gatsby-icon.png',
+  headerLinks: [
+    {
+      label: 'Home',
+      url: '/',
+    },
+  ],
+}
---
+++ src/components/about/about.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default ({ title = 'Perfil', text = '' }) => {
+  return (
+    <div>
+      <h1>{title}</h1>
+      <p dangerouslySetInnerHTML={{ __html: text }}></p>
+    </div>
+  )
+}
